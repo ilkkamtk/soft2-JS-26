0737dbd4b6d6a4b4de1213b92a5e0b163db40fe0
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-31 11:30:19 +0300

    update

---
+++ module1/t3/t3.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// unary operator + converts to number
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
+
+const sum = 0;
+const product = 0;
+const average = 0;
+
+document.querySelector('#sum').innerHTML = `Sum: ${sum}`;
+// etc...
+
