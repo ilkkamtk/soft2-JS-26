7ec978e3510ee096db9592ea426d97d9b2bc97a2
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-14 11:28:28 +0300

    update

--- module4/t1-4/t1-4.js
+++ module4/t1-4/t1-4.js
@@ -1,13 +1,27 @@
 'use strict';
 
 const form = document.querySelector('#tv-search');
+const target = document.querySelector('#results');
 
 form.addEventListener('submit', async function(evt) {
   evt.preventDefault();
+  // clear old results
+  target.innerHTML = '';
+
   const data = new FormData(form);
   const queryString = new URLSearchParams(data).toString();
 
   const response = await fetch(form.action + '?' + queryString);
-  const shows = await response.json();
-  console.log(shows);
+  const showItems = await response.json();
+
+  for (const item of showItems) {
+    const article = document.createElement('article');
+    console.log(item);
+    console.log(item.show.name);
+    const h2 = document.createElement('h2');
+    h2.innerText = item.show.name;
+
+    article.append(h2);
+    target.append(article);
+  }
 });
